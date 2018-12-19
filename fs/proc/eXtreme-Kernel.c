654e7a96cb3bf371c07034be9a2389fd14ae3bb9
Aleksandar Divitarov <alekdev001@gmail.com>
2018-12-19 20:23:10 +0100

    change to eXtreme

--- fs/proc/eXtreme-Kernel.c
+++ fs/proc/eXtreme-Kernel.c
@@ -4,29 +4,29 @@
 #include <linux/seq_file.h>
 #include <generated/compile.h>
 
-static int psycho_kernel_proc_show(struct seq_file *m, void *v)
+static int eXtreme_kernel_proc_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "{\"kernel-name\": \"eXtremeKernel\","
 			"\"version\": \"7.0\","
-			"\"buildtime\": \"%s\"}\n", PSYCHO_KERNEL_TIMESTAMP);
+			"\"buildtime\": \"%s\"}\n", eXtreme_KERNEL_TIMESTAMP);
 	return 0;
 }
 
-static int psycho_kernel_proc_open(struct inode *inode, struct file *file)
+static int eXtreme_kernel_proc_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, psycho_kernel_proc_show, NULL);
+	return single_open(file, eXtreme_kernel_proc_show, NULL);
 }
 
-static const struct file_operations psycho_kernel_proc_fops = {
-	.open		= psycho_kernel_proc_open,
+static const struct file_operations eXtreme_kernel_proc_fops = {
+	.open		= eXtreme_kernel_proc_open,
 	.read		= seq_read,
 	.llseek		= seq_lseek,
 	.release	= single_release,
 };
 
-static int __init proc_psycho_kernel_init(void)
+static int __init proc_eXtreme_kernel_init(void)
 {
-	proc_create("eXtreme-Kernel", 0, NULL, &psycho_kernel_proc_fops);
+	proc_create("eXtreme-Kernel", 0, NULL, &eXtreme_kernel_proc_fops);
 	return 0;
 }
-module_init(proc_psycho_kernel_init);
+module_init(proc_eXtreme_kernel_init);
